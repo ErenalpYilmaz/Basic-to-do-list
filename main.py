296962a28ab58fcd1f69af79637524acb53b4d74
ERENALP YILMAZ <erenalp@alpbilisim.com.tr>
2024-08-07 17:59:28 +0300

    Listele methodu olusturuldu. Ekrana TO-DO-LIST geliyor.

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-
-
 todolist = []
 
 #Listeyi yazdir.
 def listele():
+    print("TO-DO-LIST :")
     for liste in todolist:
-        print(liste)
-
+        print(f"{todolist.index(liste)+1}. {liste}")
+    print("\n\n")
 def add():
-    pass
+    mission = input("Gorevi giriniz : ")
+    todolist.append(mission)
 
 def delete():
     pass
@@ -29,6 +29,10 @@ def islemler():
             case 3:
                 delete()
             case 4:
-                pass
+                return False
     except:
         print("Hatali islem yapildi.")
+        return False
+
+while True :
+    islemler()
